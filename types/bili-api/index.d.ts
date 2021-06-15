c1d2e3b15974249a2756a95dc7f7405b561f4d5a
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-06-15 22:13:06 +0800

    cleaned up

--- types/bili-api/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "bili-api" {
-  export default function biliAPI(
-    input: { bvid?: string; aid?: string | number; mid?: string | number },
-    require: string[]
-  ): Promise<any>;
-}
