b9264b4d89218a53bc2f4ecbbf5ad480b0015968
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-06-16 11:11:40 +0800

    完成视频更新逻辑

--- routers/subscribeUpdateRouter.ts
+++ routers/subscribeUpdateRouter.ts
@@ -1,6 +1,9 @@
 /* eslint-disable consistent-return */
 import { getUnixTime } from "date-fns";
 import express from "express";
+import getUperInfo from "../api/getUperInfo";
+import getUserSpace from "../api/getUserSpace";
+import addVideos from "../controllers/addVideos";
 import Uper from "../models/Uper";
 import Video from "../models/Video";
 
@@ -40,6 +43,24 @@ subscribeUpdateRouter.get("/getUpdate/:id", async (req, res, next) => {
     .where("uper", req.params.id)
     .gte("created", uperInDB.lastUpdate);
   res.json(newVideos);
+});
+
+subscribeUpdateRouter.get("/updateVideos/:id", async (req, res, next) => {
+  const uperInDB = await Uper.findById(req.params.id);
+  if (uperInDB === null) {
+    return next({ code: 404, message: `[404] Not Found ${req.params.id}` });
+  }
+  const getUperInfoRes = await getUperInfo(uperInDB.mid);
+  const getUperInfoResData = getUperInfoRes.data;
+  if (getUperInfoResData.archive_count === uperInDB.archive_count) {
+    const noUpdateRes = { id: uperInDB._id, updates: 0 };
+    return res.json(noUpdateRes);
+  }
+  const updateCount = getUperInfoResData.archive_count - uperInDB.archive_count;
+  const getUserSpaceRes = await getUserSpace(uperInDB.mid);
+  uperInDB.archive_count = getUperInfoResData.archive_count;
+  const savedUper = await addVideos(getUserSpaceRes.list.vlist, uperInDB);
+  res.json({ newUper: savedUper, updates: updateCount });
 });
 
 export default subscribeUpdateRouter;
