53e5ee6e79d664d07cf951f953a02f8f22cdb37f
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-06-20 19:39:41 +0800

    删除返回信息中不必要的项

--- models/Uper.ts
+++ models/Uper.ts
@@ -18,6 +18,7 @@ uperSchema.set("toJSON", {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
+    delete returnedObject.subscriber;
   },
 });
 
