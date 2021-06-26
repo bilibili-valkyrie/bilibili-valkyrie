09bf0455d8c2a0789842b088a5412530cef9ee52
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-06-26 20:03:08 +0800

    bug fixed

--- routers/biliAPIRouter.ts
+++ routers/biliAPIRouter.ts
@@ -10,12 +10,12 @@ const biliAPIRouter = express.Router();
 
 require("express-async-errors");
 
-biliAPIRouter.get("/getUperinfo:mid", async (req, res) => {
+biliAPIRouter.get("/getUperinfo/:mid", async (req, res) => {
   const getUperInfoRes = await getUperInfo(req.params.mid);
   res.json(getUperInfoRes);
 });
 
-biliAPIRouter.get("/getUperSpace:mid", async (req, res) => {
+biliAPIRouter.get("/getUperSpace/:mid", async (req, res) => {
   const pn = req.query.pn ? Number(req.query.pn) : undefined;
   const ps = req.query.ps ? Number(req.query.ps) : undefined;
   const getUperSpaceRes = await getUperSpace(req.params.mid, pn, ps);
